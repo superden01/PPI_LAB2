5ba596ed23d32acf175c1f3e76e12c7b5f2b064c
superden01 <73470502+superden01@users.noreply.github.com>
2023-03-02 16:58:38 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laba_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
